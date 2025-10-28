babdfd467079cdd2e966e4e8b8b5cb1d8f8b0b75
LordBasuphan <sauljackm@gmail.com>
2025-10-28 18:21:28 +0000

    finished age logic

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random 
 
-name = input("Enter a name for your character:")
+#User input
+# name = input("Enter a name for your character:")
 
-number_of = int(input("How many would you like to create:"))
+# number_of = int(input("How many would you like to create:"))
 
+#Atrribute lists
 gender_list = ["male", "female", "non-binary"]
 
 personality_list = ["Confident", "Ambitious", "Calm", "Courageous", "Creative", "Adaptable", "Humble", "Cheerful", "Curious", "Patient", "Analytical", "Agreeable", "Adventurous","Loyal", "Compassionate", "Imaganitive", "Affectionate", "Capable"]
@@ -13,11 +15,11 @@ physical_list = ["Ugly", "Fat", "Scrawny", "Weak", "Strong", "Handsome", "Pretty
 occupation_list = ["Bard", "Theif", "Chemist", "Hunter", "Mage", "Alchemist", "Cleric", "Diplomat", "Investigator", "Knight", "Archer", "Cook", "Wizard"]
 
 
+
+#Height/Race
 height = random.uniform(3.0,7.2)
 rounded_height = round(height, 2)
 #use rounded_height as the main variable for this
-
-
 race = ""
 if rounded_height >= 5.0:
     race = "Human"
@@ -25,3 +27,14 @@ elif rounded_height >= 6.3:
     race = "Elf"
 elif rounded_height <= 5 and rounded_height >= 3.0:
     race = "Dwarf" 
+print(race)
+
+#Age
+age = 0
+if race == "Human":
+    age = random.randrange(18,100)
+elif race == "Elf":
+    age = random.randrange(18,150)
+elif race == "Dwarf":
+    age = random.randrange(18,220)
+print(age)

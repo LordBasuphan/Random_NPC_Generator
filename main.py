74457ce43734c7e4612179274d8952df2e69816c
LordBasuphan <sauljackm@gmail.com>
2025-10-30 14:16:29 +0000

    Hello

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import random 
 
+#User input
+# name = input("Enter a name for your character:")
 #User input- input from user for certian aspects of the program
 number_of = int(input("How many would you like to create:"))
 
+# number_of = int(input("How many would you like to create:"))
+
+#Atrribute lists
 #Atrribute lists- list of all the atributes needed
 gender_list = ["male", "female", "non-binary"]
 
@@ -11,6 +16,9 @@ personality_list = ["Confident", "Ambitious", "Calm", "Courageous", "Creative", 
 physical_list = ["Ugly", "Fat", "Scrawny", "Weak", "Strong", "Handsome", "Pretty", "Petite", "Athletic", "Hairy", "Bald", "Long-haired", "Short-haried", "Slender", "Stocky"]
 
 occupation_list = ["Bard", "Theif", "Chemist", "Hunter", "Mage", "Alchemist", "Cleric", "Diplomat", "Investigator", "Knight", "Archer", "Cook", "Wizard"]
+
+
+
     
 #Loops/Print
 for i in range(number_of):
@@ -19,12 +27,23 @@ for i in range(number_of):
     rounded_height = round(height, 1)
 #use rounded_height as the main variable for this
     race = ""
-    if rounded_height >= 5.0 and rounded_height <= 6.2:
+    if rounded_height >= 5.1 and rounded_height <= 6.1:
         race = "Human"
-    elif rounded_height >= 6.3:
+    elif rounded_height >= 6.2:
         race = "Elf"
     elif rounded_height <= 5 and rounded_height >= 3.0:
         race = "Dwarf" 
+
+
+#Age
+    age = 0
+    if race == "Human":
+        age = random.randrange(18,100)
+    elif race == "Elf":
+        age = random.randrange(18,150)
+    elif race == "Dwarf":
+        age = random.randrange(18,220)
+
 #Age, algorithm to determine the age of a npc
     age = 0
     if race == "Human":
@@ -34,4 +53,4 @@ for i in range(number_of):
     elif race == "Dwarf":
         age = random.randrange(18,220)
     name = input("Provide a name for the NPC:")
-    print(f"{name} is a" + age " year old",random.choice(physical_list).lower() + " and",random.choice(personality_list).lower(),random.choice(gender_list) + " " + race + "." + " They are a", random.choice(occupation_list).lower() + " and are", str(rounded_height) + " feet tall.")
+    print(f"{name} is a", age, "year old", random.choice(physical_list).lower() + " and",random.choice(personality_list).lower(),random.choice(gender_list) + " " + race + "." + " They are a", random.choice(occupation_list).lower() + " and are", str(rounded_height) + " feet tall.")
